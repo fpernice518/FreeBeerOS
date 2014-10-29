8873eee4d3b6d2660b19e1f1828a912bc79f3ebc
christian <maverickx5105@gmail.com>
2014-10-29 19:46:37 -0400

    add ticket

---
+++ nachos/kernel/threads/Ticket.java
@@ -0,0 +1,36 @@
+package nachos.kernel.threads;
+
+public class Ticket
+{
+    private int ticketNumber;
+    private boolean inUse;
+
+    Ticket(int ticketNumber){
+        this.ticketNumber = ticketNumber;
+    }
+    Ticket(int ticketNumber, boolean inuse){
+        this.ticketNumber = ticketNumber;
+        this.inUse = inuse;
+    }
+    public int getTicketNumber()
+    {
+        return ticketNumber;
+    }
+
+    public void setTicketNumber(int ticketNumber)
+    {
+        this.ticketNumber = ticketNumber;
+    }
+
+    public boolean isInUse()
+    {
+        return inUse;
+    }
+
+    public void setInUse(boolean inUse)
+    {
+        this.inUse = inUse;
+    }
+    
+   
+}
