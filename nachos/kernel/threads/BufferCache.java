3b824e243fad0389d2131b3662b0f97b115a3d29
fpernice <Frank.Pernice@stonybrook.edu>
2014-11-18 14:46:20 -0500

    uhh...well...

--- nachos/kernel/threads/BufferCache.java
+++ nachos/kernel/threads/BufferCache.java
@@ -1,51 +1,71 @@
 package nachos.kernel.threads;
 
 import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
-public class BufferCache
+public class BufferCache extends LinkedHashMap<Integer, Buffer>
 {
-   Lock cacheLock;
-   LinkedHashMap<Integer, Buffer> cache;
-   
-   Buffer getBuffer(int sectorNumber)
-   {
-       cacheLock.acquire();
-       boolean contained = cache.containsKey(sectorNumber);
-       Buffer buffer;
-       
-       if(contained == true)
-       {
-           buffer = cache.get(sectorNumber);
-           buffer.reserve();
-           cacheLock.release();
-           return buffer;           
-       }
-       else
-       {
-           buffer = replaceBuffer();
-           buffer.setSector(sectorNumber);
-           cache.put(sectorNumber, buffer);
-           cacheLock.release();
-           return buffer;
-       }
-   }
+    private static final long serialVersionUID = 1L;
+    Lock cacheLock;
+    public static final int MAX_SIZE = 10;
 
-private Buffer replaceBuffer()
-{
-    Buffer lru = new Buffer(); //TODO fix this
-    //TODO get LRU buffer
-    
-    lru.reserve();
-    
-    if(lru.isValid())
+    public BufferCache()
     {
-        cacheLock.release();
-        lru.writeBack();
-        lru.invalidate();
-        cacheLock.acquire();
-        cache.remove(lru.getSector());  //TODO not sure about this yet
+        super(MAX_SIZE, 0.75f, true);
+        cacheLock = new Lock("Cache Lock");
     }
-    
-    return lru;
-}
+
+    Buffer getBuffer(int sectorNumber)
+    {
+        cacheLock.acquire();
+        boolean contained = this.containsKey(sectorNumber);
+        Buffer buffer;
+
+        if (contained == true)
+        {
+            buffer = this.get(sectorNumber);
+            buffer.reserve();
+            cacheLock.release();
+            return buffer;
+        } else
+        {
+            buffer = replaceBuffer();
+            buffer.setSector(sectorNumber);
+            this.put(sectorNumber, buffer);
+            cacheLock.release();
+            return buffer;
+        }
+    }
+
+    private Buffer replaceBuffer()
+    {
+        try
+        {
+        Buffer lru = this.entrySet().iterator().next().getValue();
+        lru.reserve();
+
+        if (lru.isValid())
+        {
+            cacheLock.release();
+            lru.writeBack();
+            lru.invalidate();
+            cacheLock.acquire();
+            this.remove(lru.getSector()); // TODO not sure about this yet
+        }
+
+        return lru;
+        }
+        catch(NoSuchElementException e)
+        {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Buffer> eldest)
+    {
+        return size() > MAX_SIZE;
+    }
 }
