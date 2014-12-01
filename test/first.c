fcb632e8c596ec9f4c458b1d832e0ffe1d2050da
christian <maverickx5105@gmail.com>
2014-12-01 18:04:45 -0500

    found a stack problem

--- test/first.c
+++ test/first.c
@@ -4,9 +4,25 @@ void recursive(int x);
 
 int main()
 {
-  OpenFileId fd;
+  int fd;
   int end= 0;
+  char str[] = "BOBS";
+  fd = Open("test/wt.txt");
+  Write(str, 5, fd);
+  Close(fd);
+
+  char str2[5];
+
+  fd = Open("test/wt.txt");
+  
+  Read(str2, 5, fd);
+ Write("creat2\n\r", 8, ConsoleOutput);
+  Write(str2, 5, ConsoleOutput);
+    Write("creat3\n\r", 8, ConsoleOutput);
+  Close(fd);
+
   recursive(end);
+
   Write("creat1\n\r", 8, ConsoleOutput);
 }
 
